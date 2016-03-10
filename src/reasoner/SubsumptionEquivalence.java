d7890b0cdee422a51c1ff44566e93356936399fb
Jon Miranda <miranda.jra@gmail.com>
2016-03-10 11:17:43 -0800

    Added documentation for SubsumptionEquivalence.

--- src/reasoner/SubsumptionEquivalence.java
+++ src/reasoner/SubsumptionEquivalence.java
@@ -2,6 +2,18 @@ package reasoner;
 
 import reasoner.expressions.Expression;
 
+/**
+ * SubsumptionEquivalence class is used to declare two Expressions as being either
+ * a subsumption or equivalent.
+ *
+ * Subsumption example: RICH_MAN subsumption PEOPLE
+ * This declares that RICH_MAN is a subset of PEOPLE.
+ *
+ * Equivlanece example: RICH_MAN equivalent RICH and MAN
+ * This declares that RICH_MAN is equal to the set of RICH intersect set of MAN.
+ *
+ * @see {@class Equivalence}.
+ */
 public class SubsumptionEquivalence {
 
   Expression lhs;
