b229a9f816382c3406cfe85ab13b12ebdece8c8e
Jon Miranda <miranda.jra@gmail.com>
2016-03-10 11:11:09 -0800

    Added documentation for Equivalence.

--- src/reasoner/Equivalence.java
+++ src/reasoner/Equivalence.java
@@ -2,13 +2,20 @@ package reasoner;
 
 import reasoner.expressions.Expression;
 
-public class Equivalence {
-
-  Expression lhs;
-  Expression rhs;
+/**
+ * Same functionality as {@class SubsumptionEquivalence} but needs to be handled separately
+ * with regard to the {@class TBox}.
+ *
+ * The TBox for "RICH_MAN equivalent MAN and RICH" should look like:
+ * RICH_MAN => [MAN and RICH]
+ * MAN and rich => RICH_MAN
+ *
+ * Whereas TBox for "RICH_MAN subsumption MAN and RICH" should look like:
+ * RICH_MAN => [MAN and RICH]
+ */
+public class Equivalence extends SubsumptionEquivalence {
 
   public Equivalence(Expression lhs, Expression rhs) {
-    this.lhs = lhs;
-    this.rhs = rhs;
+    super(lhs, rhs);
   }
 }
