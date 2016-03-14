61deab48faba2fedf64706f3edec2d14d4493b6b
sam_rez1 <sam_rezaie87@yahoo.com>
2016-03-13 18:41:11 -0700

    Updating intersect to print 'and' and union to print 'or' \n Removing toString() in IntersectUnion

--- src/reasoner/expressions/IntersectExpression.java
+++ src/reasoner/expressions/IntersectExpression.java
@@ -4,4 +4,9 @@ public class IntersectExpression extends IntersectUnionExpression {
   public IntersectExpression(Expression lhs, Expression rhs) {
     super(lhs, rhs);
   }
+
+  @Override
+  public String toString() {
+    return lhs.toString() + " AND " + rhs.toString();
+  }
 }
