5e29896742d3b8c75abcc5a11c074b11b784369e
Brian Fung <bfung11@gmail.com>
2016-03-15 01:09:32 -0700

    Adding comments to IntersectUnionExpression

--- src/reasoner/expressions/IntersectUnionExpression.java
+++ src/reasoner/expressions/IntersectUnionExpression.java
@@ -1,14 +1,38 @@
 package reasoner.expressions;
 
+/**
+ * IntersectUnionExpression is the abstract class that 
+ * {@class IntersectExpression} and {@class UnionExpression} will inherit from.
+ * 
+ * @author Brian Fung
+ * @author Jon Miranda
+ * @author Sravani Mudduluru
+ * @author Siavash Rezaie
+ */
 public class IntersectUnionExpression extends Expression {
   public Expression lhs;
   public Expression rhs;
 
+ /**
+  * Returns IntersectUnionExpression that represent the leff-hand and right-hand
+  * expressions
+  *
+  * @param lhs the expression on the left-hand side of the dot
+  * @param rhs the expression on the right-hand side of the dot
+  * @return        the ExistentialExpression that represents the concept
+  */
   public IntersectUnionExpression(Expression lhs, Expression rhs) {
     this.lhs = lhs;
     this.rhs = rhs;
   }
 
+ /**
+  * Returns whether two expressions contradict each other.  
+  *
+  * @param other the other expression to test whether this expression and the 
+  *              other expression contradicts
+  * @return      whether the two expressions contradicts
+  */
   @Override
   public boolean contradicts(Expression other) {
     if (other instanceof NotExpression) {
