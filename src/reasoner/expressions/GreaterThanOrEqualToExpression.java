b75913f9fd647d89cef6b835526934ccfa9b10ac
Brian Fung <bfung11@gmail.com>
2016-03-15 00:08:48 -0700

    Adding comments to GreaterThanOrEqualToExpression

--- src/reasoner/expressions/GreaterThanOrEqualToExpression.java
+++ src/reasoner/expressions/GreaterThanOrEqualToExpression.java
@@ -15,17 +15,39 @@ import java.util.Set;
  * @author Sravani Mudduluru
  * @author Siavash Rezaie
  */
-public class GreaterThanOrEqualToExpression extends GreaterThanEqualLessThanEqualExpression {
+public class GreaterThanOrEqualToExpression 
+  extends GreaterThanEqualLessThanEqualExpression {
+
+   /**
+  * Returns ExistentialUniversalExpression that represent the concept 
+  *
+  * @param role    
+  * @param concept the concept that the ExistensialExpression will represent
+  * @return        the ExistentialUniversalExpression that 
+  *                represents the concept
+  */
   public GreaterThanOrEqualToExpression(String role, int count) {
     super(role, count);
   }
 
+ /**
+  * Returns whether two expressions contradict each other.  
+  *
+  * @param other the other expression to test whether this expression and the 
+  *              other expression contradicts
+  * @return      whether the two expressions contradicts
+  */
   @Override
   public boolean contradicts(Set<Expression> expressions) {
     int numberInstancesOfThisRole = getNumberInstancesOfThisRole(expressions);
     return count <= numberInstancesOfThisRole;
   }
 
+ /**
+  * Returns the role and count as a String
+  *
+  * @return the role and count as a String
+  */
   @Override
   public String toString() {
     return ">=" + super.toString();
