6ebfbadd8f47c8140b10135e72cffc2b3e368fb2
Brian Fung <bfung11@gmail.com>
2016-03-15 09:12:02 -0700

    Adding comments to Main

--- src/reasoner/Main.java
+++ src/reasoner/Main.java
@@ -3,7 +3,26 @@ package reasoner;
 import reasoner.tests.ParserTest;
 import reasoner.tests.ReasonerTest;
 
+/**
+ * Main is the class that calls and runs all the test cases for parser 
+ * and the reasoner.
+ * 
+ * The parser takes each line from user input and adds it to the TBox.
+ * The reasoner then uses the TBox and sees if the query is true.
+ * 
+ * @author Brian Fung
+ * @author Jon Miranda
+ * @author Sravani Mudduluru
+ * @author Siavash Rezaie
+ */
 public class Main {
+
+	/**
+	 * The only method in class Main that runs the test cases for the parser
+	 * and the reasoner
+	 * 
+	 * @param args the arguments that can be passed in through command line
+	 */
 	public static void main(String[] args) {
 		ReasonerTest.testManIsSubsetOfPerson();
 		ReasonerTest.testAnimalSubsetOfPerson();
