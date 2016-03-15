3051fb65362faa7bae8aff6fd39ccca130bffcb2
Brian Fung <bfung11@gmail.com>
2016-03-14 22:21:09 -0700

    Adding comments to ExistentialUniversalExpression

--- src/reasoner/expressions/ExistentialUniversalExpression.java
+++ src/reasoner/expressions/ExistentialUniversalExpression.java
@@ -6,11 +6,25 @@ import java.util.Set;
 /**
  * ExistentialUniversalExpression is super class to both
  * {@class ExistentialExpression} and {@class UniversalExpression}.
+ * It adds base functionality that is common to both of the classes.
+ * 
+ * @author Brian Fung
+ * @author Jon Miranda
+ * @author Sravani Mudduluru
+ * @author Siavash Rezaie
  */
 public class ExistentialUniversalExpression extends Expression {
   public String role;
   public HashSet<Expression> expressions;
 
+ /**
+  * Returns ExistentialUniversalExpression that represent the concept 
+  *
+  * @param role    
+  * @param concept the concept that the ExistensialExpression will represent
+  * @return        the ExistentialUniversalExpression that 
+  *                represents the concept
+  */
   protected ExistentialUniversalExpression(String role, Expression concept) {
     this.role = role;
     expressions = new HashSet<>();
@@ -21,7 +35,13 @@ public class ExistentialUniversalExpression extends Expression {
     expressions.add(concept);
   }
 
-
+ /**
+  * Returns whether two concept expressions contradict each other  
+  *
+  * @param other the other expression to test whether this expression and the 
+  *              other expression contradicts
+  * @return      whether the two expressions contradicts
+  */
   @Override
   public boolean contradicts(Expression other) {
     if (other instanceof ExistentialUniversalExpression) {
@@ -41,11 +61,24 @@ public class ExistentialUniversalExpression extends Expression {
     return false;
   }
 
+ /**
+  * Returns whether two concept expressions are equal each other  
+  *
+  * @param other the other expression to test whether this expression and the 
+  *              other expression are equal
+  * @return      whether the two expressions are equal
+  */
   @Override
   public boolean equals(Object other) {
     throw new RuntimeException("equals should be handled by subclasses ExistentialExpression or UniversalExpression");
   }
 
+ /**
+  * Returns the hash code of the concept. This hash code will be used for the 
+  * hash map(s) in the TBox.
+  *
+  * @return the hash code of the concept
+  */
   @Override
   public int hashCode() {
     int hashCode = role.hashCode();
@@ -55,6 +88,11 @@ public class ExistentialUniversalExpression extends Expression {
     return hashCode;
   }
 
+ /**
+  * Returns the concept as a String
+  *
+  * @return the concept as a String
+  */
   @Override
   public String toString() {
     StringBuilder sb = new StringBuilder();
@@ -68,6 +106,12 @@ public class ExistentialUniversalExpression extends Expression {
     return sb.toString();
   }
 
+  /**
+   * 
+   * 
+   * @param expressions the set of expressions to be added to another set of
+   *                    of expressions
+   */
   public void addConcepts(Set<Expression> expressions) {
     this.expressions.addAll(expressions);
   }
