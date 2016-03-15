ab42ede1ee670319c1f755f7e6b8f84ee37ba52c
Brian Fung <bfung11@gmail.com>
2016-03-14 21:10:03 -0700

    Adding comments to BottomExpression

--- src/reasoner/expressions/BottomExpression.java
+++ src/reasoner/expressions/BottomExpression.java
@@ -1,4 +1,13 @@
 package reasoner.expressions;
 
+/**
+ * BottomExpression represents a Bottom Expression in Description Logic.
+ *
+ * @author Brian Fung
+ * @author Jon Miranda
+ * @author Sravani Mudduluru
+ * @author Siavash Rezaie
+ */
+
 public class BottomExpression extends Expression {
 }
