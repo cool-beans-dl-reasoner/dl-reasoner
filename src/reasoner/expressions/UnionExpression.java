b4612cc6fad64dda3175b34d6ae4a2e3cca8d583
Jon Miranda <miranda.jra@gmail.com>
2016-03-15 22:40:52 -0700

    Edited UnionExpression javadoc.

--- src/reasoner/expressions/UnionExpression.java
+++ src/reasoner/expressions/UnionExpression.java
@@ -1,7 +1,7 @@
 package reasoner.expressions;
 
 /**
- * UnionExpression represents the union (or) expression in description logic
+ * UnionExpression represents the union (or) expression in description logic.
  *
  * @author Brian Fung
  * @author Jon Miranda
@@ -11,22 +11,18 @@ package reasoner.expressions;
 public class UnionExpression extends IntersectUnionExpression {
 
  /**
-  * Returns UnionExpression that contains the left-hand expression and the 
-  * right-hand expression split by the union
+  * Constructs a new UnionExpression of the two expressions.
   *
   * @param lhs the expression on the left-hand side of the union
   * @param rhs the expression on the right-hand side of the union
-  * @return        the UnionExpression that represents the concept
   */
   public UnionExpression(Expression lhs, Expression rhs) {
     super(lhs, rhs);
   }
 
- /**
-  * Returns the left-hand expression and the right-hand expression as a String
-  *
-  * @return the left-hand expression and the right-hand expression as a String
-  */
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public String toString() {
     return lhs.toString() + " OR " + rhs.toString();
