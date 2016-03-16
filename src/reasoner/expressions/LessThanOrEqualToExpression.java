efbbea8389b5c7c3105fa2586ae0e43b54cffdaa
Jon Miranda <miranda.jra@gmail.com>
2016-03-15 22:39:09 -0700

    Edited LessThanOrEqualToExpression javadoc.

--- src/reasoner/expressions/LessThanOrEqualToExpression.java
+++ src/reasoner/expressions/LessThanOrEqualToExpression.java
@@ -3,7 +3,7 @@ package reasoner.expressions;
 import java.util.Set;
 
 /**
- * LessThanOrEqualToExpression represents greater than or equal to in
+ * LessThanOrEqualToExpression represents less than or equal to in
  * description logic
  * 
  * @author Brian Fung
@@ -11,38 +11,32 @@ import java.util.Set;
  * @author Sravani Mudduluru
  * @author Siavash Rezaie
  */
-public class LessThanOrEqualToExpression 
-  extends NumberRestrictionExpression {
+public class LessThanOrEqualToExpression extends NumberRestrictionExpression {
 
- /**
-  * Returns LessThanOrEqualToExpression that represent the concept 
-  * less than or equal to
-  * 
-  * e.g. "<= 2.goto" where goto is the role and 2 is the count
-  *
-  * @param role    
-  * @param concept the concept that the LessThanOrEqualToExpression 
-  *                will represent
-  * @return        the LessThanOrEqualToExpression that 
-  *                represents the concept
-  */
+  /**
+   * Constructs a new GreaterThanOrEqualToExpression object.
+   *
+   * e.g. "<= 2.goto" where goto is the role and 2 is the count
+   *
+   * @param role The role.
+   * @param count The number restriction.
+   */
   public LessThanOrEqualToExpression(String role, int count) {
     super(role, count);
   }
 
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public boolean contradicts(Set<Expression> expressions) {
     int numberInstancesOfThisRole = getNumberInstancesOfThisRole(expressions);
     return count >= numberInstancesOfThisRole;
   }
 
- /**
-  * Returns whether two expressions contradict each other.  
-  *
-  * @param other the other expression to test whether this expression and the 
-  *              other expression contradicts
-  * @return      whether the two expressions contradicts
-  */
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public String toString() {
     return "<=" + super.toString();
